55c6e3104a8515f66f78b77c3a242a46df08bb95
QuSSucka <as960185@gmail.com>
2026-03-06 19:53:41 +0300

    Перенес lab_1_1

---
+++ lab_3_1/Date.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include <string>
+using namespace std;
+
+class Date {
+private:
+    int day;
+    int month;
+    int year;
+
+    bool isLeap(int y) const;
+    int daysInMonth(int m, int y) const;
+    bool isValid(int d, int m, int y) const;
+    void setCurrentDate();
+
+public:
+    Date();                      
+    Date(int d, int m, int y);    
+    Date(const string& str);     
+
+    void print() const;
+
+    int getDay() const;
+    int getMonth() const;
+    int getYear() const;
+
+    void setDay(int d);
+    void setMonth(int m);
+    void setYear(int y);
+
+};
